a0406bb7c6971944a82b600f5a629ff72a2c9593
Siarra Hirose <siarralh@gmail.com>
2023-06-18 15:53:31 +1000

    second commit

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -5,11 +5,10 @@ from battery.battery import Battery
 
 class SpindlerBattery(Battery, ABC):
     def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
-        self.current_date = current_date
+        super.__init__(last_service_date, current_date)
 
     def needs_service(self):
-        if self.warning_light_is_on:
+        if self.last_service_date - self.current_date >= 2:
             return True
         else:
             return False
